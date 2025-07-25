2ea8126be9c31f19d525c18b9440aa25c86ffe25
khus759 <khushikumarigupta@email.com>
2025-07-25 17:35:54 +0530

    images of real estate section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 import { setupFormValidation } from './form.js';
+import { initPropertySwiper } from './property.js';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   setupFormValidation();
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  initPropertySwiper();
+});
+
